397618c50e0dbbbe1a1d3e2c9e4c064d896b51a4
md-jamirul <mdjamirul802@gmail.com>
2023-03-31 17:05:48 +0600

    first-home-sider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
+import Home from "./component/Home";
+
 function App() {
   return (
     <div className="">
-      <div>
-        md jamirul kha so we have to know how to long be able what to do some
-        thing about me for long be country along be long be colorfully thanks
-        for any color how to long be ablw for this girls very sweet and very
-        this girls eyes very mayabi for any
-      </div>
+      <Home />
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import {FaBars,FaTimes, FaGithub,FaLinkedin,} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
+
+const Home = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className=' '>
+       <div className='flex justify-between h-screen w-full px-3 py-3  bg-[#0a192f] '>
+        <div className='font-farmm text-pink-600 text-4xl '>
+          <h1>md jamirul</h1>
+        </div>
+        <div className=''>
+        <ul className='hidden md:flex text-gray-300 gap-4   '>
+        <li className='text-lg capitalize hover:cursor-pointer hover:text-pink-600 font-semibold transition-all duration-300'>
+          <Link to='home'  duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='text-lg capitalize hover:cursor-pointer hover:text-pink-600 font-semibold transition-all duration-300'>
+          <Link to='about'  duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='text-lg capitalize hover:cursor-pointer hover:text-pink-600 font-semibold transition-all duration-300'>
+          <Link to='skills'  duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='text-lg capitalize hover:cursor-pointer hover:text-pink-600 font-semibold transition-all duration-300'>
+          <Link to='work'  duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='text-lg capitalize hover:cursor-pointer hover:text-pink-600 font-semibold transition-all duration-300'>
+          <Link to='contact'  duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+        </div>
+        
+       </div>
+        
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6  text-4xl'>
+          <Link onClick={handleClick} to='home'  duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6  text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about'  duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6  text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills'  duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6  text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work'  duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6  text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact'  duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] px-3 duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] px-3 duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] px-3 duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] px-3 duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
